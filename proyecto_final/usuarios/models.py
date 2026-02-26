69ae3e9a4057f9e6c96d5d035be7c567c4ae80db
DavidN1210 <davidnietoheras592@gmail.com>
2026-02-26 20:40:49 +0100

    commit #1

--- proyecto_final/usuarios/models.py
+++ proyecto_final/usuarios/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cliente {self.user.username}"
+    
+
+class Arquitecto(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    clientes = models.ManyToManyField(
+        Cliente,
+        related_name='arquitectos'
+    )
+
+    def __str__(self):
+        return f"Arquitecto {self.user.username}"
