7e5cf8e9e22769d982faddad95479bb0293b353f
DavidN1210 <davidnietoheras592@gmail.com>
2026-03-21 18:56:40 +0100

    Add files via upload

--- proyecto_final/subcripciones/models.py
+++ proyecto_final/subcripciones/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from usuarios.models import Cliente
 
 # Create your models here.
+class Suscripcion(models.Model):
+    TIPO_CHOICES = [
+        ('Basico', 'Básico'),
+        ('Premium', 'Premium'),
+        ('Estándar', 'Estándar'),
+    ]
+
+    id_suscripcion = models.AutoField(primary_key=True, verbose_name="ID Suscripción")
+    tipo_suscripcion = models.CharField(
+        max_length=50,
+        choices=TIPO_CHOICES,
+        blank=False,
+        verbose_name="Tipo de Suscripción"
+    )
+    precio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)],
+        blank=False,
+        verbose_name="Precio"
+    )
+    nif_cliente = models.ForeignKey(
+        'usuarios.Cliente',
+        on_delete=models.RESTRICT,
+        verbose_name="Cliente"
+    )
